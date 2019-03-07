f073d9210462733a645c1c40ba1dd2a061900e6b
EC2 Default User <ec2-user@ip-172-31-25-54.us-west-2.compute.internal>
2019-03-07 16:52:42 +0000

    update index.html to show the ICDC icon

--- img/copy.sh
+++ img/copy.sh
@@ -3,5 +3,6 @@ docker cp logo.png $1:/data-portal/src/img
 docker cp dog.png $1:/data-portal/src/img
 docker cp nci_dctd.png $1:/data-portal/src/img
 docker cp bundle.js $1:/data-portal/
-docker cp __init__.py $2:/sheepdog/src/gdcdatamodel/gdcdatamodel/models/
+docker cp index.html $1:/data-portal/
+docker cp icdc_icon.ico $1:/data-portal/src/img/favicon.ico
 
