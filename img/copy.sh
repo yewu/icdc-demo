9244484d475d0c1b10ae39078b0369da35a518d1
Cloud User <centos@ip-172-31-53-249.ec2.internal>
2019-03-14 15:52:34 +0000

    Updated footer to match the mockup

--- img/copy.sh
+++ img/copy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 docker cp logo.png $1:/data-portal/src/img
+docker cp ncilogo.png $1:/data-portal/src/img
+docker cp nci_logo.svg $1:/data-portal/src/img
 docker cp dog.png $1:/data-portal/src/img
 docker cp nci_dctd.png $1:/data-portal/src/img
 docker cp bundle.js $1:/data-portal/
